dec2676af2c773d5427488f31c4f27ec3c70dc94
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-09-15 19:59:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,4 +6,5 @@ formElement.addEventListener("submit",changeTheText);
 function changeTheText(event){
 	event.preventDefault();
 	paragraphElement.innerText=`https://localhost:8080/?name=${event.target[0].value} & year=${event.target[1].value}`
+	formElement.reset()
 }
