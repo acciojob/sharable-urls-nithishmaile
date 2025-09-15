ab32fab508d00c9c2244e46ea1392ed536a36817
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-09-15 19:38:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// your code here
+const formElement=document.getElementById("myForm");
+const paragraphElement=document.getElementById("paragraph");
+
+formElement.addEventListener("submit",changeTheText);
+
+function changeTheText(event){
+	event.preventDefault();
+	paragraphElement.innerText=`https://localhost:8080/?name=${event.target[0].value} & year=${event.target[1].value}`
+}
